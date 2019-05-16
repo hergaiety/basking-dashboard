cd2ebf6643d2ae444cc28f9ed02ff256dd7faa8a
Joe Wroten <joe@wroten.me>
2019-05-16 17:11:09 -0500

    build script for server

--- mkRaw.sh
+++ mkRaw.sh
@@ -3,6 +3,8 @@ mkdir -p data/json
 
 touch data/raw/real/date.raw
 date > data/raw/real/date.raw
+echo "date.raw updated"
 
 touch data/raw/real/diskspace.raw
 df | grep /dev/md0 > data/raw/real/diskspace.raw
+echo "diskspace.raw updated"

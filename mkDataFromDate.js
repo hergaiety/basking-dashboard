66967bd8a977a0d1bc69f3a74637d5cb226849c7
Joe Wroten <joe@wroten.me>
2019-05-16 17:27:45 -0500

    prettier time rendered

--- mkDataFromDate.js
+++ mkDataFromDate.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const moment = require('moment');
 
 module.exports = async function(useTestData = false) {
   fs.readFile(path.join(__dirname, 'data/raw', useTestData ? 'test' : 'real', 'date.raw'), 'utf8', (err, date) => {
@@ -10,7 +11,7 @@ module.exports = async function(useTestData = false) {
     date = new Date(date.slice(0, date.length - 1));
     let jsonString = JSON.stringify({
       raw: date,
-      pretty: date.toDateString(),
+      pretty: moment(date).format('hh:mm MM-DD-YYYY'),
     });
 
     fs.writeFile(path.join(__dirname, 'data/json', 'date.json'), jsonString, err => {
